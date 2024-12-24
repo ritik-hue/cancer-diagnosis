884c585ae0d6c9c43fcc8f63e045cc12f21b4efd
Ritik Singh <111752319+ritik-hue@users.noreply.github.com>
2024-12-24 14:21:33 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -166,7 +166,7 @@ def add_predictions(input_data):
 
 def main():
   st.set_page_config(
-    page_title="Cancer Predictor",
+    page_title="Breast Cancer Predictor",
     page_icon=":female-doctor:",
     layout="wide",
     initial_sidebar_state="expanded"
@@ -178,7 +178,7 @@ def main():
   input_data = add_sidebar()
   
   with st.container():
-    st.title("Breast Cancer Predictor")
+    st.title("Cancer Predictor")
     st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
   
   col1, col2 = st.columns([4,1])
