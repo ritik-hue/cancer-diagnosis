9469282d8c25ec68caea8b00e99d4545ff6a11fc
Ritik Singh <111752319+ritik-hue@users.noreply.github.com>
2024-12-24 14:23:04 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -179,7 +179,7 @@ def main():
   
   with st.container():
     st.title("Breast Cancer Predictor")
-    st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
+    st.write("Please connect this app to your cytology lab to help diagnose breast cancer from your tissue sample. This app predicts using a machine learning model whether a tissue mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
   
   col1, col2 = st.columns([4,1])
   
